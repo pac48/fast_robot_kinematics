ec3be51388f09bdf4306424d1cd43cbba8321ec2
Paul Gesel <paul.gesel@picknik.ai>
2024-01-28 18:26:28 -0700

    refactor joint data struct
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>

--- include/forward_kinematics_eigen.hpp
+++ include/forward_kinematics_eigen.hpp
@@ -5,58 +5,90 @@
 
 namespace fast_fk {
   namespace internal {
+    // input_data: sin(t) cos(t)  px py pz R11, R12, R13...
     void forward_kinematics_internal(double *input_data);
+
     constexpr size_t joint_data_length = 17;
   }
 
   struct JointData {
-    std::array<double, internal::joint_data_length> joint_data;
+    std::array<std::array<double, internal::joint_data_length>, FAST_FK_NUMBER_OF_JOINTS> joint_data = {0};
 
-    void set_joint(double value) {
-      joint_data[0] = std::sin(value);
-      joint_data[1] = std::cos(value);
+    void set_joint(size_t ind, double value) {
+      joint_data[ind][0] = std::sin(value);
+      joint_data[ind][1] = std::cos(value);
     }
 
     void set_joints(const double *values) {
-      for (auto i = 0; i < FAST_FK_NUMBER_OF_JOINTS; ++i) {
-        joint_data[i * internal::joint_data_length] = std::sin(values[i]);
-        joint_data[i * internal::joint_data_length + 1] = std::cos(values[i]);
+      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+        joint_data[ind][0] = std::sin(values[ind]);
+        joint_data[ind][1] = std::cos(values[ind]);
       }
     }
 
-    double get_joint(size_t ind) {
-      return asin(joint_data[ind * 17]);
-    }
-
-    void get_joints(double *values) {
-      for (auto i = 0; i < FAST_FK_NUMBER_OF_JOINTS; ++i) {
-        values[i] = asin(joint_data[i * internal::joint_data_length]);
+    void set_joints(const Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS> &values) {
+      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+        joint_data[ind][0] = std::sin(values[ind]);
+        joint_data[ind][1] = std::cos(values[ind]);
       }
     }
 
-    void set_joint(double sin_t, double cos_t) {
-      joint_data[0] = sin_t;
-      joint_data[1] = cos_t;
+    void set_joint(size_t ind, double sin_t, double cos_t) {
+      joint_data[ind][0] = sin_t;
+      joint_data[ind][1] = cos_t;
     }
 
     void set_joints(const double *sin_values, const double *cos_values) {
-      for (auto i = 0; i < FAST_FK_NUMBER_OF_JOINTS; ++i) {
-        joint_data[i * internal::joint_data_length] = sin_values[i];
-        joint_data[i * internal::joint_data_length + 1] = cos_values[i];
+      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+        joint_data[ind][0] = sin_values[ind];
+        joint_data[ind][1] = cos_values[ind];
       }
     }
 
-    void get_frame(){
+    void set_joints(const Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS> &sin_values,
+                    const Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS> &cos_values) {
+      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+        joint_data[ind][0] = sin_values[ind];
+        joint_data[ind][1] = cos_values[ind];
+      }
+    }
 
+
+    double get_joint(size_t ind) {
+      return asin(joint_data[ind][0]);
+    }
+
+    void get_joints(double *values) {
+      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+        values[ind] = asin(joint_data[ind][0]);
+      }
+    }
+
+
+    void get_frame(size_t index, Eigen::Matrix<double, 4, 4> &transform) {
+      transform(0, 3) = joint_data[index][2];
+      transform(1, 3) = joint_data[index][3];
+      transform(2, 3) = joint_data[index][4];
+
+      transform(0, 0) = joint_data[index][5];
+      transform(0, 1) = joint_data[index][6];
+      transform(0, 2) = joint_data[index][7];
+
+      transform(1, 0) = joint_data[index][8];
+      transform(1, 1) = joint_data[index][9];
+      transform(1, 2) = joint_data[index][10];
+
+      transform(2, 0) = joint_data[index][11];
+      transform(2, 1) = joint_data[index][12];
+      transform(2, 2) = joint_data[index][13];
+
+      transform(3, 3) = 1.0;
     }
   };
 
 
-  template<std::size_t SIZE>
-  void forward_kinematics(std::array<JointData, SIZE> &input_data) {
-    static_assert(SIZE == FAST_FK_NUMBER_OF_JOINTS,
-                  "`forward_kinematics` called with the wrong number of joints. Hint: FAST_FK_NUMBER_OF_JOINTS defines the correct number of joints.");
-    internal::forward_kinematics_internal(input_data.data()->joint_data.data());
+  void forward_kinematics(JointData &input_data) {
+    internal::forward_kinematics_internal(input_data.joint_data.data()->data());
   }
 
 }
--- test/forward_kinematics_test.cpp
+++ test/forward_kinematics_test.cpp
@@ -7,39 +7,25 @@
 #include "forward_kinematics.hpp"
 #endif
 
-double rand_double() {
-  double div = RAND_MAX / 2;
-  return -1 + (rand() / div);
-}
-
 int main(int arc, char **argv) {
-  constexpr int iterations = 128 * 128 * 128;
-  constexpr size_t num_joints = FAST_FK_NUMBER_OF_JOINTS;
-  std::array<fast_fk::JointData, num_joints> joints = {0};
-  auto start = std::chrono::high_resolution_clock::now();
-  double rand_val = 0;
-  for (int i = 0; i < iterations; i++) {
-    if ((i % 1000) == 0) {
-      srand((unsigned int) time(0));
-      rand_val = rand_double();
-    }
-    rand_val *= 0.999;
-    for (auto k = 0; k < num_joints; ++k) {
-//      joints[k].set_joint(rand_val, sqrt(1.0 - rand_val * rand_val));
-//      rand_val = 3.141592/2;
-      joints[k].set_joint(rand_val);
-    }
+  constexpr int iterations = 128 * 128;
+  std::array<Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS>, iterations> rand_values;
+  for (auto &rand_val: rand_values) {
+    rand_val = Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS>::Random();
+  }
 
+  fast_fk::JointData joints;
+  auto start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < iterations; i++) {
+    auto &rand_val = rand_values[i];
+    joints.set_joints(rand_val, (1.0 - rand_val.array() * rand_val.array()).sqrt());
     fast_fk::forward_kinematics(joints);
+    Eigen::Matrix4d transform;
+    joints.get_frame(5, transform);
   }
 
   auto stop = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
-
-//  for (auto i = 0; i < num_joints; ++i) {
-//    std::cout << i << ": final value: " << joints[i].joint_data[4] << std::endl;
-//    i++;
-//  }
 
   std::cout << "Time taken by function: " << (double) duration.count() << " nanoseconds" << std::endl;
   std::cout << "Average: " << ((double) duration.count()) / (iterations) << " nanoseconds"
