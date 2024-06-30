9da03f425fba8b24c2d8bf846b9b71d806246a0a
Paul Gesel <paul.gesel@picknik.ai>
2024-06-30 10:48:54 -0600

    Add inverse kinematics support (#1)

--- cmake/fast_forward_kinematics-extras.cmake
+++ cmake/fast_forward_kinematics-extras.cmake
@@ -7,6 +7,18 @@ function(generate_fast_forward_kinematics_library URDF_FILE ROOT_LINK TIP_LINK)
     message(FATAL_ERROR "Python interpreter not found.")
   endif ()
 
+  include(ExternalProject)
+  ExternalProject_Add(
+      LBFGSpp
+      PREFIX ${CMAKE_BINARY_DIR}/LBFGSpp
+      GIT_REPOSITORY https://github.com/yixuan/LBFGSpp.git
+      GIT_TAG master
+      CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}
+  )
+  ExternalProject_Get_Property(LBFGSpp source_dir)
+  set(LBFGSppIncludeDir ${source_dir}/include)
+
+
   execute_process(
       COMMAND ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/scripts/robot_gen.py ${URDF_FILE} ${CMAKE_SOURCE_DIR}/scripts/robot_config.cpp.template ${CMAKE_CURRENT_BINARY_DIR}/forward_kinematics_lib.cpp ${ROOT_LINK} ${TIP_LINK}
       OUTPUT_VARIABLE FAST_FK_NUMBER_OF_JOINTS
@@ -32,7 +44,9 @@ function(generate_fast_forward_kinematics_library URDF_FILE ROOT_LINK TIP_LINK)
   option(USE_EIGEN "Enables Eigen types in the generated library" ON)
   find_package(Eigen3 3.3 NO_MODULE)
   if(Eigen3_FOUND AND USE_EIGEN)
+    add_dependencies(fast_forward_kinematics_library LBFGSpp)
     target_link_libraries(fast_forward_kinematics_library PUBLIC Eigen3::Eigen)
+    target_include_directories(fast_forward_kinematics_library PUBLIC ${LBFGSppIncludeDir})
     target_compile_definitions(fast_forward_kinematics_library PUBLIC FAST_FK_USE_EIGEN)
   endif ()
 
--- include/forward_kinematics_eigen.hpp
+++ include/forward_kinematics_eigen.hpp
@@ -1,98 +1,100 @@
 #ifndef FAST_FORWARD_KINEMATICS_HPP
 #define FAST_FORWARD_KINEMATICS_HPP
 
+#include "LBFGS.h"
 #include <Eigen/Core>
 
 namespace fast_fk {
-  namespace internal {
-    // input_data: sin(t) cos(t)  px py pz R11, R12, R13...
-    void forward_kinematics_internal(double *input_data, size_t size);
+    namespace internal {
+        // input_data: sin(t) cos(t)  px py pz R11, R12, R13...
+        void forward_kinematics_internal(double *input_data, size_t size);
 
-    constexpr size_t joint_data_length = 17;
-  }
-
-  struct JointData {
-    std::array<std::array<double, internal::joint_data_length>, FAST_FK_NUMBER_OF_JOINTS> joint_data = {0};
-
-    void set_joint(size_t ind, double value) {
-      joint_data[ind][0] = std::sin(value);
-      joint_data[ind][1] = std::cos(value);
+        constexpr size_t joint_data_length = 17;
     }
 
-    void set_joints(const double *values) {
-      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
-        joint_data[ind][0] = std::sin(values[ind]);
-        joint_data[ind][1] = std::cos(values[ind]);
-      }
+    struct JointData {
+        std::array<std::array<double, internal::joint_data_length>, FAST_FK_NUMBER_OF_JOINTS> joint_data = {0};
+
+        void set_joint(size_t ind, double value) {
+            joint_data[ind][0] = std::sin(value);
+            joint_data[ind][1] = std::cos(value);
+        }
+
+        void set_joints(const double *values) {
+            for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+                joint_data[ind][0] = std::sin(values[ind]);
+                joint_data[ind][1] = std::cos(values[ind]);
+            }
+        }
+
+        void set_joints(const Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS> &values) {
+            for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+                joint_data[ind][0] = std::sin(values[ind]);
+                joint_data[ind][1] = std::cos(values[ind]);
+            }
+        }
+
+        void set_joint(size_t ind, double sin_t, double cos_t) {
+            joint_data[ind][0] = sin_t;
+            joint_data[ind][1] = cos_t;
+        }
+
+        void set_joints(const double *sin_values, const double *cos_values) {
+            for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+                joint_data[ind][0] = sin_values[ind];
+                joint_data[ind][1] = cos_values[ind];
+            }
+        }
+
+        void set_joints(const Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS> &sin_values,
+                        const Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS> &cos_values) {
+            for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+                joint_data[ind][0] = sin_values[ind];
+                joint_data[ind][1] = cos_values[ind];
+            }
+        }
+
+
+        [[nodiscard]] double get_joint(size_t ind) const {
+            return asin(joint_data[ind][0]);
+        }
+
+        void get_joints(double *values) const {
+            for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
+                values[ind] = asin(joint_data[ind][0]);
+            }
+        }
+
+
+        void get_frame(size_t index, Eigen::Matrix<double, 4, 4> &transform) const {
+            transform(0, 3) = joint_data[index][2];
+            transform(1, 3) = joint_data[index][3];
+            transform(2, 3) = joint_data[index][4];
+
+            transform(0, 0) = joint_data[index][5];
+            transform(0, 1) = joint_data[index][6];
+            transform(0, 2) = joint_data[index][7];
+
+            transform(1, 0) = joint_data[index][8];
+            transform(1, 1) = joint_data[index][9];
+            transform(1, 2) = joint_data[index][10];
+
+            transform(2, 0) = joint_data[index][11];
+            transform(2, 1) = joint_data[index][12];
+            transform(2, 2) = joint_data[index][13];
+
+            transform(3, 0) = 0.0;
+            transform(3, 1) = 0.0;
+            transform(3, 2) = 0.0;
+            transform(3, 3) = 1.0;
+        }
+    };
+
+
+    void forward_kinematics(JointData &input_data) {
+        internal::forward_kinematics_internal(input_data.joint_data.data()->data(),
+                                              input_data.joint_data.size() * internal::joint_data_length);
     }
-
-    void set_joints(const Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS> &values) {
-      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
-        joint_data[ind][0] = std::sin(values[ind]);
-        joint_data[ind][1] = std::cos(values[ind]);
-      }
-    }
-
-    void set_joint(size_t ind, double sin_t, double cos_t) {
-      joint_data[ind][0] = sin_t;
-      joint_data[ind][1] = cos_t;
-    }
-
-    void set_joints(const double *sin_values, const double *cos_values) {
-      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
-        joint_data[ind][0] = sin_values[ind];
-        joint_data[ind][1] = cos_values[ind];
-      }
-    }
-
-    void set_joints(const Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS> &sin_values,
-                    const Eigen::Vector<double, FAST_FK_NUMBER_OF_JOINTS> &cos_values) {
-      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
-        joint_data[ind][0] = sin_values[ind];
-        joint_data[ind][1] = cos_values[ind];
-      }
-    }
-
-
-    [[nodiscard]] double get_joint(size_t ind) const {
-      return asin(joint_data[ind][0]);
-    }
-
-    void get_joints(double *values) const {
-      for (auto ind = 0; ind < FAST_FK_NUMBER_OF_JOINTS; ++ind) {
-        values[ind] = asin(joint_data[ind][0]);
-      }
-    }
-
-
-    void get_frame(size_t index, Eigen::Matrix<double, 4, 4> &transform) const {
-      transform(0, 3) = joint_data[index][2];
-      transform(1, 3) = joint_data[index][3];
-      transform(2, 3) = joint_data[index][4];
-
-      transform(0, 0) = joint_data[index][5];
-      transform(0, 1) = joint_data[index][6];
-      transform(0, 2) = joint_data[index][7];
-
-      transform(1, 0) = joint_data[index][8];
-      transform(1, 1) = joint_data[index][9];
-      transform(1, 2) = joint_data[index][10];
-
-      transform(2, 0) = joint_data[index][11];
-      transform(2, 1) = joint_data[index][12];
-      transform(2, 2) = joint_data[index][13];
-
-      transform(3, 0) = 0.0;
-      transform(3, 1) = 0.0;
-      transform(3, 2) = 0.0;
-      transform(3, 3) = 1.0;
-    }
-  };
-
-
-  void forward_kinematics(JointData &input_data) {
-    internal::forward_kinematics_internal(input_data.joint_data.data()->data(), input_data.joint_data.size()*internal::joint_data_length);
-  }
 
 }
 
