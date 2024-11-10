ebba857ea113dc7de43cd0839bebe9499df7ac36
Paul Gesel <paul.gesel@picknik.ai>
2024-11-09 21:54:15 -0700

    Add format files and CI job (#4)

--- code_generation/robot_gen.py
+++ code_generation/robot_gen.py
@@ -7,11 +7,11 @@ import numpy as np
 
 def run():
     parser = argparse.ArgumentParser()
-    parser.add_argument('urdf_file')
-    parser.add_argument('fk_template')
-    parser.add_argument('fk_output_file')
-    parser.add_argument('root_link_name')
-    parser.add_argument('tip_link_name')
+    parser.add_argument("urdf_file")
+    parser.add_argument("fk_template")
+    parser.add_argument("fk_output_file")
+    parser.add_argument("root_link_name")
+    parser.add_argument("tip_link_name")
     args = parser.parse_args()
 
     root_link_name = args.root_link_name
@@ -24,17 +24,29 @@ def run():
         xyz = joint.origin.xyz
         T = np.eye(4)
 
-        yaw = np.array([[np.cos(rpy[2]), np.sin(rpy[2]), 0],
-                        [-np.sin(rpy[2]), np.cos(rpy[2]), 0],
-                        [0, 0, 1]])
+        yaw = np.array(
+            [
+                [np.cos(rpy[2]), np.sin(rpy[2]), 0],
+                [-np.sin(rpy[2]), np.cos(rpy[2]), 0],
+                [0, 0, 1],
+            ]
+        )
 
-        pitch = np.array([[np.cos(rpy[1]), 0, np.sin(rpy[1])],
-                          [0, 1, 0],
-                          [-np.sin(rpy[1]), 0, np.cos(rpy[1])]])
+        pitch = np.array(
+            [
+                [np.cos(rpy[1]), 0, np.sin(rpy[1])],
+                [0, 1, 0],
+                [-np.sin(rpy[1]), 0, np.cos(rpy[1])],
+            ]
+        )
 
-        roll = np.array([[1, 0, 0],
-                         [0, np.cos(rpy[0]), np.sin(rpy[0])],
-                         [0, -np.sin(rpy[0]), np.cos(rpy[0])]])
+        roll = np.array(
+            [
+                [1, 0, 0],
+                [0, np.cos(rpy[0]), np.sin(rpy[0])],
+                [0, -np.sin(rpy[0]), np.cos(rpy[0])],
+            ]
+        )
         T[:3, :3] = yaw @ pitch @ roll
         T[:3, 3] = xyz
 
@@ -53,40 +65,43 @@ def run():
     T_fixed = np.eye(4)
     for joint_name in joint_names:
         joint = robot.joint_map[joint_name]
-        if joint.type == 'fixed':
+        if joint.type == "fixed":
             T_fixed = T_fixed * get_transform(joint)
-        elif joint.type == 'revolute':
+        elif joint.type == "revolute":
             # TODO: need to add joint limits?
             T = get_transform(joint) @ T_fixed
             T_fixed = np.eye(4)
             rotations.append(T[:3, :3])
             offsets.append(T[:3, 3])
-            types.append('revolute')
-        elif joint.type == 'continuous':
+            types.append("revolute")
+        elif joint.type == "continuous":
             T = get_transform(joint) @ T_fixed
             T_fixed = np.eye(4)
             rotations.append(T[:3, :3])
             offsets.append(T[:3, 3])
-            types.append('revolute')
-        elif joint.type == 'prismatic':
+            types.append("revolute")
+        elif joint.type == "prismatic":
             # TODO: need to add joint limits?
             T = get_transform(joint) @ T_fixed
             T_fixed = np.eye(4)
             rotations.append(T[:3, :3])
             offsets.append(T[:3, 3])
-            types.append('prismatic')
+            types.append("prismatic")
         else:
             raise Exception(f"joint type {joint.type} in URDF not supported")
 
     # truncate near zero values
-    offsets = [val * (np.abs(val) > 1E-5) for val in offsets]
-    rotations = [val * (np.abs(val) > 1E-5) for val in rotations]
+    offsets = [val * (np.abs(val) > 1e-5) for val in offsets]
+    rotations = [val * (np.abs(val) > 1e-5) for val in rotations]
 
-    with open(args.fk_template, 'r') as f:
+    with open(args.fk_template, "r") as f:
         j2_template = Template(f.read())
-        code = j2_template.render({'rotations': rotations, 'offsets': offsets, 'types': types}, trim_blocks=True)
+        code = j2_template.render(
+            {"rotations": rotations, "offsets": offsets, "types": types},
+            trim_blocks=True,
+        )
 
-    with open(args.fk_output_file, 'w') as f:
+    with open(args.fk_output_file, "w") as f:
         f.write(code)
 
     print(f"FAST_FK_NUMBER_OF_JOINTS={len(types)}", end="")
--- test/forward_kinematics_test.cpp
+++ test/forward_kinematics_test.cpp
@@ -11,32 +11,37 @@ using KI = fast_fk::JointData;
 using KI = kdl_impl::JointData;
 #endif
 
-int main(int arc, char **argv) {
-    constexpr int iterations = 128 * 128;
-    constexpr int multiplier = 128 * MULTIPLIER;
-    std::array<Eigen::Vector<float, KI::get_num_joints()>, iterations> rand_values;
-    for (auto &rand_val: rand_values) {
-        rand_val = Eigen::Vector<float, KI::get_num_joints()>::Random();
+int main(int arc, char** argv)
+{
+  constexpr int iterations = 128 * 128;
+  constexpr int multiplier = 128 * MULTIPLIER;
+  std::array<Eigen::Vector<float, KI::get_num_joints()>, iterations> rand_values;
+  for(auto& rand_val : rand_values)
+  {
+    rand_val = Eigen::Vector<float, KI::get_num_joints()>::Random();
+  }
+
+  fk_interface::ForwardKinematicsInterface<KI> fk_interface;
+  Eigen::Matrix<float, 4, 4> tf;
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  for(int k = 0; k < multiplier; k++)
+  {
+    for(int i = 0; i < iterations; i++)
+    {
+      fk_interface.set_joints(rand_values[i]);
+      fk_interface.forward_kinematics();
     }
+  }
 
-    fk_interface::ForwardKinematicsInterface<KI> fk_interface;
-    Eigen::Matrix<float, 4, 4> tf;
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
 
-    auto start = std::chrono::high_resolution_clock::now();
+  std::cout << "Time taken by function: " << (float)duration.count() << " nanoseconds"
+            << std::endl;
+  std::cout << "Average: " << ((float)duration.count()) / (iterations * multiplier)
+            << " nanoseconds" << std::endl;
 
-    for (int k = 0; k < multiplier; k++) {
-        for (int i = 0; i < iterations; i++) {
-            fk_interface.set_joints(rand_values[i]);
-            fk_interface.forward_kinematics();
-        }
-    }
-
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
-
-    std::cout << "Time taken by function: " << (float) duration.count() << " nanoseconds" << std::endl;
-    std::cout << "Average: " << ((float) duration.count()) / (iterations * multiplier) << " nanoseconds"
-              << std::endl;
-
-    return 0;
+  return 0;
 }
