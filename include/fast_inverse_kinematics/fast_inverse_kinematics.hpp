0a4daf7da94e63b0f541e6e2832d3b60c20e1672
Paul Gesel <paul.gesel@picknik.ai>
2024-11-09 21:53:04 -0700

    format files
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>

--- include/fast_inverse_kinematics/fast_inverse_kinematics.hpp
+++ include/fast_inverse_kinematics/fast_inverse_kinematics.hpp
@@ -1,74 +1,81 @@
 #pragma once
 
-#include "memory"
-
 #include <Eigen/Core>
 #include <Eigen/Dense>
+
+#include "memory"
 
 #ifdef FAST_FK_USE_IK
 
 #include "LBFGS.h"
-
 #include "kinematics_interface.hpp"
 
-namespace fast_fk::internal {
-    class InverseKinematics {
-    public:
-        Eigen::Matrix<float, 3, 3> target_rot_;
-        Eigen::Vector<float, 3> target_pose_;
-        std::array<std::array<float, 16>, FAST_FK_NUMBER_OF_JOINTS> joint_data = {0};
-        std::unique_ptr<LBFGSpp::LBFGSSolver<float>> solver;
-        LBFGSpp::LBFGSParam<float> param;
+namespace fast_fk::internal
+{
+class InverseKinematics
+{
+public:
+  Eigen::Matrix<float, 3, 3> target_rot_;
+  Eigen::Vector<float, 3> target_pose_;
+  std::array<std::array<float, 16>, FAST_FK_NUMBER_OF_JOINTS> joint_data = { 0 };
+  std::unique_ptr<LBFGSpp::LBFGSSolver<float>> solver;
+  LBFGSpp::LBFGSParam<float> param;
 
-        InverseKinematics(const Eigen::Matrix<float, 3, 3> &target_rot,
-                          const Eigen::Vector<float, 3> &target_pose) : target_rot_{target_rot},
-                                                                        target_pose_{target_pose} {
-            param.epsilon = 1E-3;
-            param.epsilon_rel = 1E-3;
-            param.max_iterations = 30;
+  InverseKinematics(const Eigen::Matrix<float, 3, 3>& target_rot,
+                    const Eigen::Vector<float, 3>& target_pose)
+    : target_rot_{ target_rot }, target_pose_{ target_pose }
+  {
+    param.epsilon = 1E-3;
+    param.epsilon_rel = 1E-3;
+    param.max_iterations = 30;
 
-            solver = std::make_unique<LBFGSpp::LBFGSSolver<float >>(param);
-        }
+    solver = std::make_unique<LBFGSpp::LBFGSSolver<float>>(param);
+  }
 
-        fk_interface::IKSolverStats
-        inverse_kinematics(Eigen::Matrix<float, 4, 4> &transform, Eigen::VectorX<float> &q_guess) {
-            target_rot_ = transform.block<3, 3>(0, 0);
-            target_pose_(0) = transform(0, 3);
-            target_pose_(1) = transform(1, 3);
-            target_pose_(2) = transform(2, 3);
+  fk_interface::IKSolverStats inverse_kinematics(Eigen::Matrix<float, 4, 4>& transform,
+                                                 Eigen::VectorX<float>& q_guess)
+  {
+    target_rot_ = transform.block<3, 3>(0, 0);
+    target_pose_(0) = transform(0, 3);
+    target_pose_(1) = transform(1, 3);
+    target_pose_(2) = transform(2, 3);
 
-            float fx = 1E10;
-            int niter;
+    float fx = 1E10;
+    int niter;
 
-            try {
-                niter = solver->minimize(*this, q_guess, fx);
-            } catch (const std::runtime_error &e) {
-                return {fx, niter, solver->final_grad_norm(), false, e.what()};
-            }
+    try
+    {
+      niter = solver->minimize(*this, q_guess, fx);
+    }
+    catch(const std::runtime_error& e)
+    {
+      return { fx, niter, solver->final_grad_norm(), false, e.what() };
+    }
 
-            return {fx, niter, solver->final_grad_norm(), true, ""};
-        }
+    return { fx, niter, solver->final_grad_norm(), true, "" };
+  }
 
-        float operator()(const Eigen::VectorX<float> &q, Eigen::VectorX<float> &grad);
-
-
-    };
-}
+  float operator()(const Eigen::VectorX<float>& q, Eigen::VectorX<float>& grad);
+};
+}  // namespace fast_fk::internal
 #else
-namespace fast_fk::internal {
-    class InverseKinematics {
-    public:
-        InverseKinematics(const Eigen::Matrix<float, 3, 3> &target_rot,
-                                             const Eigen::Vector<float, 3> &target_pose){}
+namespace fast_fk::internal
+{
+class InverseKinematics
+{
+public:
+  InverseKinematics(const Eigen::Matrix<float, 3, 3>& target_rot,
+                    const Eigen::Vector<float, 3>& target_pose)
+  {}
 
-        fk_interface::IKSolverStats
-        inverse_kinematics(Eigen::Matrix<float, 4, 4> &transform, Eigen::VectorX<float> &q_guess) {
-            throw std::logic_error("Function not implemented.");
-            return {};
-        }
+  fk_interface::IKSolverStats inverse_kinematics(Eigen::Matrix<float, 4, 4>& transform,
+                                                 Eigen::VectorX<float>& q_guess)
+  {
+    throw std::logic_error("Function not implemented.");
+    return {};
+  }
 
-        float operator()(const Eigen::VectorX<float> &q, Eigen::VectorX<float> &grad);
-
-    };
-}
+  float operator()(const Eigen::VectorX<float>& q, Eigen::VectorX<float>& grad);
+};
+}  // namespace fast_fk::internal
 #endif
--- test/inverse_kinematics_test.cpp
+++ test/inverse_kinematics_test.cpp
@@ -11,55 +11,57 @@ using IK = fast_fk::JointData;
 using IK = kdl_impl::JointData;
 #endif
 
+int main(int arc, char** argv)
+{
+  unsigned seed = time(0);
+  srand(seed);
+  rand();
 
-int main(int arc, char **argv) {
-    unsigned seed = time(0);
-    srand(seed);
-    rand();
+  constexpr int iterations = 128 * 128 * 5 * MULTIPLIER;
 
-    constexpr int iterations = 128 * 128 * 5* MULTIPLIER;
+  // get target pose
+  Eigen::VectorX<float> q_in = Eigen::VectorX<float>::Random(IK::get_num_joints());
+  fk_interface::InverseKinematicsInterface<IK> fk_interface;
+  fk_interface.set_joints(q_in);
+  fk_interface.forward_kinematics();
+  Eigen::Matrix<float, 4, 4> tf;
+  fk_interface.get_frame(IK::get_num_joints() - 1, tf);
 
-    // get target pose
-    Eigen::VectorX<float> q_in = Eigen::VectorX<float>::Random(IK::get_num_joints());
-    fk_interface::InverseKinematicsInterface<IK> fk_interface;
-    fk_interface.set_joints(q_in);
-    fk_interface.forward_kinematics();
-    Eigen::Matrix<float, 4, 4> tf;
-    fk_interface.get_frame(IK::get_num_joints() - 1, tf);
+  auto start = std::chrono::high_resolution_clock::now();
 
-    auto start = std::chrono::high_resolution_clock::now();
+  fk_interface::IKSolverStats stats;
+  size_t failed = 0;
+  size_t succeeded = 0;
 
-    fk_interface::IKSolverStats stats;
-    size_t failed = 0;
-    size_t succeeded = 0;
+  Eigen::VectorX<float> q = 1 * Eigen::VectorX<float>::Random(IK::get_num_joints());
+  for(int ind = 0; ind < iterations; ++ind)
+  {
+    q = Eigen::VectorX<float>::Random(IK::get_num_joints());
+    stats = fk_interface.inverse_kinematics(tf, q);
+    failed += stats.success == 0;
+    succeeded += stats.success == 1;
+    //        if (!stats.success) {
+    // debug info
+    //            std::cout << " failed!!" << std::endl;
+    //            std::cout << "q: " << q.transpose() << std::endl;
+    //            std::cout << "q_in: " << q_in.transpose() << std::endl;
+    //            std::cout << stats.niter << " iterations" << std::endl;
+    //            std::cout << "f(x) = " << stats.fx << std::endl;
+    //            std::cout << "||grad|| = " << stats.grad_norm << std::endl;
+    //            std::cout << "error: " << stats.what << std::endl;
+    //            continue;
+    //        }
+  }
 
-    Eigen::VectorX<float> q = 1 * Eigen::VectorX<float>::Random(IK::get_num_joints());
-    for (int ind = 0; ind < iterations; ++ind) {
-        q = Eigen::VectorX<float>::Random(IK::get_num_joints());
-        stats = fk_interface.inverse_kinematics(tf, q);
-        failed += stats.success == 0;
-        succeeded += stats.success == 1;
-//        if (!stats.success) {
-        // debug info
-//            std::cout << " failed!!" << std::endl;
-//            std::cout << "q: " << q.transpose() << std::endl;
-//            std::cout << "q_in: " << q_in.transpose() << std::endl;
-//            std::cout << stats.niter << " iterations" << std::endl;
-//            std::cout << "f(x) = " << stats.fx << std::endl;
-//            std::cout << "||grad|| = " << stats.grad_norm << std::endl;
-//            std::cout << "error: " << stats.what << std::endl;
-//            continue;
-//        }
-    }
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+  std::cout << "Time taken by function: " << (double)duration.count() << " microseconds"
+            << std::endl;
+  std::cout << "Average: " << ((double)duration.count()) / (iterations) << " microseconds"
+            << std::endl;
+  std::cout << "Percent success: " << ((double)(100.0 * succeeded) / (failed + succeeded))
+            << "%" << std::endl;
 
-    std::cout << "Time taken by function: " << (double) duration.count() << " microseconds" << std::endl;
-    std::cout << "Average: " << ((double) duration.count()) / (iterations) << " microseconds"
-              << std::endl;
-    std::cout << "Percent success: " << ((double) (100.0 * succeeded) / (failed + succeeded)) << "%"
-              << std::endl;
-
-    return 0;
+  return 0;
 }
